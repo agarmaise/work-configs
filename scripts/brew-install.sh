40b51d4f770029c17f544533780f3d3a86f2e43f
Avery Garmaise <avery.garmaise@unity3d.com>
2026-01-15 11:55:45 -0500

    Update initial install script

--- scripts/brew-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-where brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install bash coreutils docker findutils gh git gnu-sed grep jq tmux tmuxinator util-linux wget yarn
-brew install --cask alt-tab font-sauce-code-pro-nerd-font iterm2 raycast scroll-reverser visual-studio-code
---
+++ scripts/initial-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+where brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install bash coreutils docker findutils gh git gnu-sed grep jq todo-txt tmux tmuxinator util-linux wget yarn
+brew install --cask alt-tab font-sauce-code-pro-nerd-font iterm2 raycast scroll-reverser visual-studio-code
