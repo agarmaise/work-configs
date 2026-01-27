ca7a2505305ed06d906e88392b7fd1e9cccad5d5
Avery Garmaise <avery.garmaise@unity3d.com>
2026-01-27 13:26:09 -0500

    Add rectangle to init script

--- scripts/initial-install.sh
+++ scripts/initial-install.sh
@@ -7,4 +7,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 where brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install bash coreutils docker findutils gh git gnu-sed grep jq todo-txt tmux tmuxinator util-linux wget yarn
-brew install --cask alt-tab font-sauce-code-pro-nerd-font iterm2 raycast scroll-reverser visual-studio-code
+brew install --cask alt-tab font-sauce-code-pro-nerd-font iterm2 raycast rectangle scroll-reverser visual-studio-code
