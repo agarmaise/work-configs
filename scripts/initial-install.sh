4854b3f1ecde1c60106b75e8b50926387dea9221
Avery Garmaise <avery.garmaise@unity3d.com>
2026-03-30 10:22:57 -0400

    Update initialize script

--- scripts/initial-install.sh
+++ scripts/initial-install.sh
@@ -1,10 +1,35 @@
-#!/bin/bash
+#!/bin/zsh -i
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+which omz &> /dev/null || zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+which nvm &> /dev/null || curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | zsh
 
-where brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew &> /dev/null || zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install bash coreutils docker findutils gh git gnu-sed grep jq todo-txt tmux tmuxinator util-linux wget yarn
-brew install --cask alt-tab font-sauce-code-pro-nerd-font iterm2 keyclu raycast rectangle scroll-reverser visual-studio-code
+brew install \
+  bash \
+  coreutils \
+  docker \
+  findutils \
+  gh \
+  git \
+  gnu-sed \
+  grep \
+  jq \
+  ripgrep \
+  tmux \
+  tmuxinator \
+  todo-txt \
+  util-linux \
+  wget
+
+brew install --cask \
+  alt-tab \
+  docker-desktop \
+  font-sauce-code-pro-nerd-font \
+  iterm2 \
+  keyclu \
+  raycast \
+  rectangle \
+  scroll-reverser \
+  visual-studio-code
